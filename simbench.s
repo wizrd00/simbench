722f7d42757c104c82ac5eb55e1951dd2821c6cd
wizrd <wizrd.bin@proton.me>
2026-02-17 18:34:21 +0330

    the program works fine

--- simbench.s
+++ simbench.s
@@ -1,3 +1,9 @@
+	.data
+msg :
+	.ascii "\nScore -> "
+lnf :
+	.ascii "\n\n"
+
 	.bss
 buf :
 	.skip 0x14
@@ -12,13 +18,13 @@ _start :
 	mov $0xc9, %rax
 	syscall
 	mov %rax, %rbx
-	mov $0xfffffffff, %rcx
-	jmp loop
-loop :
+	mov $0x5ffffffff, %rcx
+	jmp loop0
+loop0 :
 	cmp $0x00, %rcx
 	je out
 	sub $0x01, %rcx
-	jmp loop
+	jmp loop0
 out :
 	mov $0xc9, %rax
 	syscall
@@ -30,19 +36,39 @@ print :
 	xor %rdx, %rdx
 	mov $0x0a, %rbx
 	divq %rbx
-	add $'0', %rbx
+	add $'0', %rdx
 	mov %rdx, (%rsi, %rcx, 1)
-	sub $0x01, %rcx
 	cmp $0x00, %rax
-	je exit
-	jmp revbuf
+	je revbuf
+	add $0x01, %rcx
+	jmp print
 revbuf :
-	
+	mov %rcx, %rdx
+	xor %rcx, %rcx
+	jmp loop1
+loop1 :
+	cmp %rcx, %rdx
+	jle exit
+	mov (%rsi, %rcx, 1), %rax
+	mov (%rsi, %rdx, 1), %rbx
+	mov %rax, (%rsi, %rdx, 1)
+	mov %rbx, (%rsi, %rcx, 1)
+	add 0x01, %rcx
+	sub 0x01, %rdx
+	jmp loop1
 exit :
 	mov $0x01, %rax
 	mov $0x01, %rdi
-	mov buf, %rsi
+	mov $msg, %rsi
+	mov $0x0a, %rdx
+	syscall
+	mov $0x01, %rax
+	mov $buf, %rsi
 	mov $0x14, %rdx
+	syscall
+	mov $0x01, %rax
+	mov $lnf, %rsi
+	mov $0x02, %rdx
 	syscall
 	mov $0x3c, %rax
 	mov $0x00, %rdi
