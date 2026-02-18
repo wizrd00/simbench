18b51ec17184b3b650cadf3ef51a9c600aeac6c9
wizrd <wizrd.bin@proton.me>
2026-02-18 13:02:46 +0330

    remove some horrible bugs, and add tiny error handling

--- simbench.s
+++ simbench.s
@@ -1,6 +1,8 @@
 	.data
 msg :
 	.ascii "\nScore -> "
+err :
+	.ascii "\nError on sys_time"
 lnf :
 	.ascii "\n\n"
 
@@ -14,11 +16,13 @@ buf :
 _start :
 	xor %rax, %rax
 	xor %rcx, %rcx
-	xor %rdi, %rdi
 	mov $0xc9, %rax
+	xor %rdi, %rdi
 	syscall
+	cmp $-0x1, %rax
+	je error
 	mov %rax, %rbx
-	mov $0x6ffffffff, %rcx
+	mov $0xfffffffff, %rcx
 	jmp loop0
 loop0 :
 	cmp $0x00, %rcx
@@ -27,17 +31,20 @@ loop0 :
 	jmp loop0
 out :
 	mov $0xc9, %rax
+	xor %rdi, %rdi
 	syscall
+	cmp $-0x01, %rax
+	je error
 	sub %rbx, %rax
 	mov $buf, %rsi
-	mov $0x00, %rcx
+	xor %rcx, %rcx
 	jmp print
 print :
 	xor %rdx, %rdx
 	mov $0x0a, %rbx
 	divq %rbx
-	add $'0', %rdx
-	mov %rdx, (%rsi, %rcx, 1)
+	add $'0', %dl
+	movb %dl, (%rsi, %rcx, 1)
 	cmp $0x00, %rax
 	je revbuf
 	add $0x01, %rcx
@@ -48,15 +55,15 @@ revbuf :
 	jmp loop1
 loop1 :
 	cmp %rcx, %rdx
-	jle exit
-	mov (%rsi, %rcx, 1), %rax
-	mov (%rsi, %rdx, 1), %rbx
-	mov %rax, (%rsi, %rdx, 1)
-	mov %rbx, (%rsi, %rcx, 1)
-	add 0x01, %rcx
-	sub 0x01, %rdx
+	jle inact
+	movb (%rsi, %rcx, 1), %al
+	movb (%rsi, %rdx, 1), %bl
+	movb %al, (%rsi, %rdx, 1)
+	movb %bl, (%rsi, %rcx, 1)
+	add $0x01, %rcx
+	sub $0x01, %rdx
 	jmp loop1
-exit :
+inact :
 	mov $0x01, %rax
 	mov $0x01, %rdi
 	mov $msg, %rsi
@@ -70,6 +77,15 @@ exit :
 	mov $lnf, %rsi
 	mov $0x02, %rdx
 	syscall
+	jmp exit
+error :
+	mov $0x01, %rax
+	mov $0x01, %rdi
+	mov %err, %rsi
+	mov %0xf2, %rdx
+	syscall
+	jmp exit
+exit :
 	mov $0x3c, %rax
 	mov $0x00, %rdi
 	syscall
